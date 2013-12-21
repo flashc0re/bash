176252e080d685e190d387ea8def994d02292638
flashc0re <flashc0re@root.je>
2013-12-21 00:52:27 +0000

    Paintfull index changer

---
+++ pic.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Paintfull index changer 0.1 by flash0re
+# Check & Change index.html / index.php / index.asp / index.cfm to your file and replace it every minute
+# Usage: pic.sh -f "FILE" -l "local file" -r "remote patch(http://host/my_index.php"
