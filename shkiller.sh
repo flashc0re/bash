4669d931e038840b6b202a308077a490bd6f49c8
flashc0re <flashc0re@root.je>
2013-12-21 00:45:58 +0000

    Php Shell Finder

--- shkiller.sh
+++ shkiller.sh
@@ -4,8 +4,8 @@
 # Usage: shkiller.sh "DIR TO CHECK"
 
 dir="$1"
-echo -e "\e[01;32mMethod #1 90% Rate(encoded)\e[00m"
+echo -e "\e[01;32mMethod #1 90% Rate (encoded)\e[00m"
 grep -r eval.*base64 $dir >> shkiller.log 
-echo -e "\e[01;32mMethod #2 FilesMan\e[00m"
+echo -e "\e[01;32mMethod #2 FilesMan (uncoded, example: WSO)\e[00m"
 grep -r FilesMan $dir >> shkiller.log
 echo -e "\e[01;32mDone results saved: shkiller.log \e[00m"
