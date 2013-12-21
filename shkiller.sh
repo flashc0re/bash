8defa0b0570c114c7ea3aa61898f37059d264503
flashc0re <flashc0re@root.je>
2013-12-21 00:09:21 +0000

    shell finder

---
+++ shkiller.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Shell killer 0.1 by flash0re
+# Simple script to check for web shells
+# Usage: shkiller.sh "DIR TO CHECK"
+
+dir="$1"
+echo -e "\e[01;32mMethod #1 90% Rate\e[00m"
+grep -r eval.*base64 $dir >> shkiller.log
+echo -e "\e[01;32mDone results saved: shkiller.log \e[00m"
